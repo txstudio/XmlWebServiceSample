2790e41a31e603b82b6d0c78a5c646bd6f4ae29a
Shi Hao Wang <st890409@hotmail.com>
2017-12-27 10:57:13 +0800

    Initial commit

---
+++ WebApp/UserService.asmx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebApp
+{
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // 若要允許使用 ASP.NET AJAX 從指令碼呼叫此 Web 服務，請將下面這行文字取消註解。 
+    // [System.Web.Script.Services.ScriptService]
+    public class UserService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public bool AddUser(string account, string email, string description)
+        {
+            return (true);
+        }
+    }
+}
